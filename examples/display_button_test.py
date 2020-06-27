35e56c1e68e04e5e52a4ac61109484c1db1e23f7
FoamyGuy <foamyguy@gmail.com>
2020-06-27 14:41:36 -0500

    adding touch to button test

--- examples/display_button_test.py
+++ examples/display_button_test.py
@@ -1,6 +1,17 @@
 import board
 import displayio
 from display_layouts.absolute_layout import AbsoluteLayout
+import adafruit_touchscreen
+
+# Setup touchscreen (PyPortal)
+ts = adafruit_touchscreen.Touchscreen(
+    board.TOUCH_XL,
+    board.TOUCH_XR,
+    board.TOUCH_YD,
+    board.TOUCH_YU,
+    calibration=((5200, 59000), (5800, 57000)),
+    size=(320, 240),
+)
 
 f = open("layouts/button_test.json", "r")
 layout_str = f.read()
@@ -9,5 +20,13 @@ main_layout = AbsoluteLayout(board.DISPLAY, layout_str)
 
 board.DISPLAY.show(main_layout.view)
 
+_button = main_layout.sub_view_by_id("main_btn").button
+
+# Loop and look for touches
 while True:
-    pass
+    p = ts.touch_point
+    if p:
+        if _button.contains(p):
+            _button.selected = True
+    else:
+        _button.selected = False
